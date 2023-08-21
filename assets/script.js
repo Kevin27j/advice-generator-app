f0c70e099c60ffdf5559442a0c5ea15973b4357b
Kejvin Pashaj <kevin.952011@gmail.com>
2023-08-21 17:46:48 +0100

    api data fetch

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,15 @@
-function displayRandomAdvice() {
-    
+const adviceId = document.getElementById("advice-id");
+const adviceContent = document.getElementById("advice-content");
+const api_url = "https://api.adviceslip.com/advice";
+
+async function getRandomAdvice() {
+    const response = await fetch(api_url);
+    let data = await response.json();
+    console.log(typeof data)
+    const id = data.slip.id;
+    const advice = data.slip.advice;
+    console.log(id, advice)
+
 }
+
+getRandomAdvice()
