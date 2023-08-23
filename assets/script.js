fa46f22ae2b8130029ff65fda2eb815afc073d4d
Kejvin Pashaj <kevin.952011@gmail.com>
2023-08-23 15:29:45 +0100

    display advice id and text

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,27 @@
 const adviceId = document.getElementById("advice-id");
 const adviceContent = document.getElementById("advice-content");
+const randomButton = document.getElementById("random-button");
+
 const api_url = "https://api.adviceslip.com/advice";
 
 async function getRandomAdvice() {
+    // Fetch response data and save it in new variable
     const response = await fetch(api_url);
     let data = await response.json();
-    console.log(typeof data)
+
+    // Save random id and advice to new variables
     const id = data.slip.id;
     const advice = data.slip.advice;
-    console.log(id, advice)
-
+    // console.log(id, advice)
+    handleClick(id, advice);
 }
+
+function handleClick(id, advice) {
+    // console.log(id, advice)
+    adviceId.innerHTML = id
+    adviceContent.innerText = advice
+}
+
+randomButton.addEventListener("click", handleClick)
 
 getRandomAdvice()
